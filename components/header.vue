483b8e52a965dd42f9bafe057f5ede0c0e28e08b
Danillo Fonseca <danillo.fonseca.129@ufrn.edu.br>
2025-04-21 22:11:53 -0300

    feat: create header

---
+++ components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="w-full bg-gray-800">
+    <div class="flex w-full items-center justify-center py-4">
+      <img
+        src="/assets/images/svg/logo.svg"
+        alt="Logo"
+        class="max-h-[60px] max-w-[200px] object-contain"
+      />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped></style>
